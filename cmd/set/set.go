52063c798056d148da2f9e31016a5f4f7c2180ac
Neccolini <shun11202991@gmail.com>
2023-03-01 03:08:39 +0900

    update: use original set. outputs network png file. fix: Solving the problem of overlapping node positions in internal processing by using set.

---
+++ cmd/set/set.go
@@ -0,0 +1,36 @@
+package set
+
+import (
+	"math/rand"
+)
+
+type Set[T comparable] struct {
+	hashMap map[T]struct{}
+}
+
+func NewSet[T comparable](t T) *Set[T] {
+	return &Set[T]{map[T]struct{}{t: {}}}
+}
+
+func (s *Set[T]) Add(t T) {
+	s.hashMap[t] = struct{}{}
+}
+
+func (s *Set[T]) Remove(t T) {
+	delete(s.hashMap, t)
+}
+
+func (s *Set[T]) Size() int {
+	return len(s.hashMap)
+}
+
+func (s *Set[T]) RandomChoice() T {
+	randomKey := func() T {
+		keys := make([]T, 0, len(s.hashMap))
+		for k := range s.hashMap {
+			keys = append(keys, k)
+		}
+		return keys[rand.Intn(len(keys))]
+	}()
+	return randomKey
+}
